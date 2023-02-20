9261aa66c31ac420c84aba9b4ffce442b1b2e5f7
Mystic Quest <l1skou@gmail.com>
2023-02-20 03:13:22 +0200

    adds vertices to the grid

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -8,22 +8,37 @@ public class Grid : MonoBehaviour
     public int xSize, ySize;
 
     private Vector3[] vertices;
+    private Mesh mesh;
 
     private void Awake()
     {
-        Generate();
+        StartCoroutine(Generate());
     }
 
-    private void Generate()
+    private IEnumerator Generate()
     {
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
         for (int i = 0, y = 0; y <= ySize; y++)
         {
             for (int x = 0; x <= xSize; x++, i++)
             {
                 vertices[i] = new Vector3(x, y);
+                yield return wait;
             }
         }
+
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[3];
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = xSize + 1;
+        mesh.triangles = triangles;
     }
 
     private void OnDrawGizmos()
