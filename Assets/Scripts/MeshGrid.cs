7d929f8670ebdd41d09df9c75b9cd8c06467c06d
Mystic Quest <l1skou@gmail.com>
2023-02-20 03:42:24 +0200

    creates triangle vertices on our grid vertices

--- Assets/Scripts/MeshGrid.cs
+++ Assets/Scripts/MeshGrid.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
-public class Grid : MonoBehaviour
+public class MeshGrid : MonoBehaviour
 {
 
     public int xSize, ySize;
@@ -28,17 +28,24 @@ public class Grid : MonoBehaviour
             for (int x = 0; x <= xSize; x++, i++)
             {
                 vertices[i] = new Vector3(x, y);
-                yield return wait;
             }
         }
 
         mesh.vertices = vertices;
 
-        int[] triangles = new int[3];
-        triangles[0] = 0;
-        triangles[1] = 1;
-        triangles[2] = xSize + 1;
-        mesh.triangles = triangles;
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+                mesh.triangles = triangles;
+                yield return wait;
+            }
+        }
     }
 
     private void OnDrawGizmos()
