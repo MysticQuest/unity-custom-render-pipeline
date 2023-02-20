16157ca5739e21381d0b35b2c67240f98e28a716
Mystic Quest <l1skou@gmail.com>
2023-02-20 05:50:24 +0200

    rp asset and initialization

---
+++ Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline 
+{
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    { }
+
+}
