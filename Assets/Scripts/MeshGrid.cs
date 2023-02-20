e6bd593cfd267429bc4754973ace23d07ffea56b
Mystic Quest <l1skou@gmail.com>
2023-02-20 05:04:15 +0200

    adds uv and normal map

--- Assets/Scripts/MeshGrid.cs
+++ Assets/Scripts/MeshGrid.cs
@@ -12,26 +12,46 @@ public class MeshGrid : MonoBehaviour
 
     private void Awake()
     {
-        StartCoroutine(Generate());
+        Generate();
     }
 
-    private IEnumerator Generate()
+    private void Generate()
     {
-        WaitForSeconds wait = new WaitForSeconds(0.05f);
-
+        // Creates mesh and adds it to the filder
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
         mesh.name = "Procedural Grid";
 
+        // Creates our mesh grid vertices
+        // sets the UV texture coords to the mesh vertices
+        // and the tagents for the normal map
+
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+
         for (int i = 0, y = 0; y <= ySize; y++)
         {
             for (int x = 0; x <= xSize; x++, i++)
             {
                 vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                tangents[i] = tangent;
             }
         }
-
         mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
+
+        CreateMeshTriangles();
+
+        mesh.RecalculateNormals();
+    }
+
+    private void CreateMeshTriangles()
+    {
+        // Creates triagle vertices (counted clock-wise) on the grid mesh vertices
 
         int[] triangles = new int[xSize * ySize * 6];
         for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
@@ -42,22 +62,22 @@ public class MeshGrid : MonoBehaviour
                 triangles[ti + 3] = triangles[ti + 2] = vi + 1;
                 triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
                 triangles[ti + 5] = vi + xSize + 2;
-                mesh.triangles = triangles;
-                yield return wait;
             }
         }
+        mesh.triangles = triangles;
     }
 
-    private void OnDrawGizmos()
-    {
-        if (vertices == null)
-        {
-            return;
-        }
-        Gizmos.color = Color.black;
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            Gizmos.DrawSphere(vertices[i], 0.1f);
-        }
-    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    if (vertices == null)
+    //    {
+    //        return;
+    //    }
+    //    Gizmos.color = Color.black;
+    //    for (int i = 0; i < vertices.Length; i++)
+    //    {
+    //        Gizmos.DrawSphere(vertices[i], 0.1f);
+    //    }
+    //}
 }
