9946bf4a7e6a26f4c3b634aa5276eefa2fe6c5a2
Mystic Quest <l1skou@gmail.com>
2023-02-21 06:46:37 +0200

    adds shader library functions for the vertex translation pass

--- Assets/Custom RP/Shaders/UnlitPass.hlsl
+++ Assets/Custom RP/Shaders/UnlitPass.hlsl
@@ -1,9 +1,12 @@
 #ifndef CUSTOM_UNLIT_PASS_INCLUDED
 #define CUSTOM_UNLIT_PASS_INCLUDED
 
-float4 UnlitPassVertex (float3 positionOS : POSITION) : SV_POSITION 
+#include "../ShaderLibrary/Common.hlsl"
+
+float4 UnlitPassVertex(float3 positionOS : POSITION) : SV_POSITION
 {
-	return float4(positionOS, 1.0);
+    float3 positionWS = TransformObjectToWorld(positionOS.xyz);
+    return TransformWorldToHClip(positionWS);
 }
 
 float4 UnlitPassFragment () : SV_TARGET
