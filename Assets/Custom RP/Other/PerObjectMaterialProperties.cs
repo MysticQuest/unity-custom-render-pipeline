748ffa7172b2cb39a47024151330841033fe315a
Mystic Quest <l1skou@gmail.com>
2023-02-22 13:44:10 +0200

    adds per object material props

---
+++ Assets/Custom RP/Other/PerObjectMaterialProperties.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PerObjectMaterialProperties : MonoBehaviour
+{
+
+    static int baseColorId = Shader.PropertyToID("_BaseColor");
+    static MaterialPropertyBlock block;
+
+    [SerializeField]
+    Color baseColor = Color.white;
+
+    void Awake()
+    {
+        OnValidate();
+    }
+
+    void OnValidate()
+    {
+        if (block == null)
+        {
+            block = new MaterialPropertyBlock();
+        }
+        block.SetColor(baseColorId, baseColor);
+        GetComponent<Renderer>().SetPropertyBlock(block);
+    }
+}
