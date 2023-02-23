0b57d83e455d292d20f19532dd95ce4e9e780179
Mystic Quest <l1skou@gmail.com>
2023-02-23 12:06:26 +0200

    adds lighting function

--- Assets/Custom RP/ShaderLibrary/Lighting.hlsl
+++ Assets/Custom RP/ShaderLibrary/Lighting.hlsl
@@ -1,6 +1,16 @@
 #ifndef CUSTOM_LIGHTING_INCLUDED
 #define CUSTOM_LIGHTING_INCLUDED
 
+float3 IncomingLight(Surface surface, Light light)
+{
+    return saturate(dot(surface.normal, light.direction)) * light.color;
+}
+
+float3 GetLighting(Surface surface, Light light)
+{
+    return IncomingLight(surface, light) * surface.color;
+}
+
 float3 GetLighting(Surface surface)
 {
     return surface.normal.y * surface.color;
