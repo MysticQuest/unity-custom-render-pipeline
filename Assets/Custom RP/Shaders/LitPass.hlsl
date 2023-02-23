4222f8594c4f7a3b3c058f80fe5e026460e89b87
Mystic Quest <l1skou@gmail.com>
2023-02-23 11:31:23 +0200

    adds surface properties

--- Assets/Custom RP/Shaders/LitPass.hlsl
+++ Assets/Custom RP/Shaders/LitPass.hlsl
@@ -2,6 +2,7 @@
 #define CUSTOM_LIT_PASS_INCLUDED
 
 #include "../ShaderLibrary/Common.hlsl"
+#include "../ShaderLibrary/Surface.hlsl"
 
 TEXTURE2D(_BaseMap);
 SAMPLER(sampler_BaseMap);
@@ -50,8 +51,13 @@ float4 LitPassFragment(Varyings input) : SV_TARGET
     #if defined(_CLIPPING)
 		clip(base.a - UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff));
 	#endif
-    base.rgb = normalize(input.normalWS);
-    return base;
+    
+    Surface surface;
+    surface.normal = normalize(input.normalWS);
+    surface.color = base.rgb;
+    surface.alpha = base.a;
+
+    return float4(surface.color, surface.alpha);
 }
 
 #endif
