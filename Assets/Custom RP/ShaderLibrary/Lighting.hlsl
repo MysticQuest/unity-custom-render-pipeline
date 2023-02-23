5101424847479f175a9e326ca92d100e2e967f60
Mystic Quest <l1skou@gmail.com>
2023-02-23 13:30:19 +0200

    adds multiple dir lights on shaders

--- Assets/Custom RP/ShaderLibrary/Lighting.hlsl
+++ Assets/Custom RP/ShaderLibrary/Lighting.hlsl
@@ -13,7 +13,12 @@ float3 GetLighting(Surface surface, Light light)
 
 float3 GetLighting(Surface surface)
 {
-    return GetLighting(surface, GetDirectionalLight());
+    float3 color = 0.0;
+    for (int i = 0; i < GetDirectionalLightCount(); i++)
+    {
+        color += GetLighting(surface, GetDirectionalLight(i));
+    }
+    return color;
 }
 
 #endif
