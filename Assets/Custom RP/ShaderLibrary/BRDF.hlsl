384422cf865b4e78d4404442c3dde63a568dca6d
Mystic Quest <l1skou@gmail.com>
2023-02-24 12:07:53 +0200

    calculates reflectivity for metallic specular reflection

--- Assets/Custom RP/ShaderLibrary/BRDF.hlsl
+++ Assets/Custom RP/ShaderLibrary/BRDF.hlsl
@@ -1,5 +1,6 @@
 #ifndef CUSTOM_BRDF_INCLUDED
 #define CUSTOM_BRDF_INCLUDED
+#define MIN_REFLECTIVITY 0.04
 
 struct BRDF
 {
@@ -8,10 +9,17 @@ struct BRDF
     float roughness;
 };
 
+float OneMinusReflectivity(float metallic)
+{
+    float range = 1.0 - MIN_REFLECTIVITY;
+    return range - metallic * range;
+}
+
 BRDF GetBRDF(Surface surface)
 {
     BRDF brdf;
-    brdf.diffuse = surface.color;
+    float oneMinusReflectivity = OneMinusReflectivity(surface.metallic);
+    brdf.diffuse = surface.color * oneMinusReflectivity;
     brdf.specular = 0.0;
     brdf.roughness = 1.0;
     return brdf;
