f95f2f22b228abaa48abe67a2068bf6629f5b134
Mystic Quest <l1skou@gmail.com>
2023-02-24 13:12:42 +0200

    updates scene items for custom materials and draw call debug

--- Assets/Custom RP/Tests/MeshBall.cs
+++ Assets/Custom RP/Tests/MeshBall.cs
@@ -6,7 +6,14 @@ public class MeshBall : MonoBehaviour
     Vector4[] baseColors = new Vector4[1023];
 
     MaterialPropertyBlock block;
-    static int baseColorId = Shader.PropertyToID("_BaseColor");
+    static int 
+        baseColorId = Shader.PropertyToID("_BaseColor"),
+        metallicId = Shader.PropertyToID("_Metallic"),
+        smoothnessId = Shader.PropertyToID("_Smoothness");
+
+    float[]
+    metallic = new float[1023],
+    smoothness = new float[1023];
 
     [SerializeField]
     Mesh mesh = default;
@@ -22,7 +29,8 @@ public class MeshBall : MonoBehaviour
             matrices[i] = Matrix4x4.TRS(
                              Random.insideUnitSphere * 10f + center,
                              Quaternion.Euler(Random.value * 360f, Random.value * 360f, Random.value * 360f), Vector3.one * Random.Range(0.5f, 1.5f));
-            
+            metallic[i] = Random.value < 0.25f ? 1f : 0f;
+            smoothness[i] = Random.Range(0.05f, 0.95f);
             baseColors[i] = new Vector4(Random.value, Random.value, Random.value, Random.Range(0.5f, 1f));
         }
     }
@@ -33,6 +41,8 @@ public class MeshBall : MonoBehaviour
         {
             block = new MaterialPropertyBlock();
             block.SetVectorArray(baseColorId, baseColors);
+            block.SetFloatArray(metallicId, metallic);
+            block.SetFloatArray(smoothnessId, smoothness);
         }
         Graphics.DrawMeshInstanced(mesh, 0, material, matrices, 1023, block);
     }
