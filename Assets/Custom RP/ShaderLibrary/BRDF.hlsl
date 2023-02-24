21c2e49833ddceba120ca0b4bdc220651ffa4ee1
Mystic Quest <l1skou@gmail.com>
2023-02-24 13:16:32 +0200

    premultiplies alpha blending to restore specular lights in transpanrency

--- Assets/Custom RP/ShaderLibrary/BRDF.hlsl
+++ Assets/Custom RP/ShaderLibrary/BRDF.hlsl
@@ -36,6 +36,7 @@ BRDF GetBRDF(Surface surface)
     BRDF brdf;
     float oneMinusReflectivity = OneMinusReflectivity(surface.metallic);
     brdf.diffuse = surface.color * oneMinusReflectivity;
+    brdf.diffuse *= surface.alpha;
     brdf.specular = lerp(MIN_REFLECTIVITY, surface.color, surface.metallic);
     float perceptualRoughness = PerceptualSmoothnessToPerceptualRoughness(surface.smoothness);
     brdf.roughness = PerceptualRoughnessToRoughness(perceptualRoughness);
