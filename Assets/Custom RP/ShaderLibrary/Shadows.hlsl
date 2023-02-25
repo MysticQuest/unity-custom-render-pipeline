9739c43fa111bc9851cb588d51b86dc47da6209d
Mystic Quest <l1skou@gmail.com>
2023-02-25 20:16:42 +0200

    adds a way to sample the shadowmap

---
+++ Assets/Custom RP/ShaderLibrary/Shadows.hlsl
@@ -0,0 +1,16 @@
+#ifndef CUSTOM_SHADOWS_INCLUDED
+#define CUSTOM_SHADOWS_INCLUDED
+
+#define MAX_SHADOWED_DIRECTIONAL_LIGHT_COUNT 4
+
+TEXTURE2D_SHADOW(_DirectionalShadowAtlas);
+#define SHADOW_SAMPLER sampler_linear_clamp_compare
+SAMPLER_CMP(SHADOW_SAMPLER);
+
+CBUFFER_START(_CustomShadows)
+float4x4 _DirectionalShadowMatrices[MAX_SHADOWED_DIRECTIONAL_LIGHT_COUNT];
+CBUFFER_END
+
+#endif
+
+
