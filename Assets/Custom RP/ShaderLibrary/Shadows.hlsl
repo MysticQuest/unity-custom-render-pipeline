c4edf556db1155887d1da040915874a307396e09
Mystic Quest <l1skou@gmail.com>
2023-02-25 20:29:54 +0200

    adds the sampling to the shaders and attenuates shadow by fragment position

--- Assets/Custom RP/ShaderLibrary/Shadows.hlsl
+++ Assets/Custom RP/ShaderLibrary/Shadows.hlsl
@@ -11,6 +11,33 @@ CBUFFER_START(_CustomShadows)
 float4x4 _DirectionalShadowMatrices[MAX_SHADOWED_DIRECTIONAL_LIGHT_COUNT];
 CBUFFER_END
 
+struct DirectionalShadowData
+{
+    float strength;
+    int tileIndex;
+};
+
+float SampleDirectionalShadowAtlas(float3 positionSTS)
+{
+    return SAMPLE_TEXTURE2D_SHADOW(
+		_DirectionalShadowAtlas, SHADOW_SAMPLER, positionSTS
+	);
+}
+
+float GetDirectionalShadowAttenuation(DirectionalShadowData data, Surface surfaceWS)
+{
+    if (data.strength <= 0.0)
+    {
+        return 1.0;
+    }
+    float3 positionSTS = mul(
+		_DirectionalShadowMatrices[data.tileIndex],
+		float4(surfaceWS.position, 1.0)
+	).xyz;
+    float shadow = SampleDirectionalShadowAtlas(positionSTS);
+    return lerp(1.0, shadow, data.strength);
+}
+
 #endif
 
 
