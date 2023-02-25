7c32cae5d1fa94a6460f6c36d636ca5415d01780
Mystic Quest <l1skou@gmail.com>
2023-02-25 20:36:39 +0200

    gets dir shadow data to light and factors in attenuation

--- Assets/Custom RP/ShaderLibrary/Light.hlsl
+++ Assets/Custom RP/ShaderLibrary/Light.hlsl
@@ -13,6 +13,7 @@ struct Light
 {
     float3 color;
     float3 direction;
+    float attenuation;
 };
 
 int GetDirectionalLightCount()
@@ -20,11 +21,21 @@ int GetDirectionalLightCount()
     return _DirectionalLightCount;
 }
 
-Light GetDirectionalLight(int index)
+DirectionalShadowData GetDirectionalShadowData(int lightIndex)
+{
+    DirectionalShadowData data;
+    data.strength = _DirectionalLightShadowData[lightIndex].x;
+    data.tileIndex = _DirectionalLightShadowData[lightIndex].y;
+    return data;
+}
+
+Light GetDirectionalLight(int index, Surface surfaceWS)
 {
     Light light;
     light.color = _DirectionalLightColors[index].rgb;
     light.direction = _DirectionalLightDirections[index].xyz;
+    DirectionalShadowData shadowData = GetDirectionalShadowData(index);
+    light.attenuation = GetDirectionalShadowAttenuation(shadowData, surfaceWS);
     return light;
 }
 
--- Assets/Custom RP/ShaderLibrary/Lighting.hlsl
+++ Assets/Custom RP/ShaderLibrary/Lighting.hlsl
@@ -3,7 +3,9 @@
 
 float3 IncomingLight(Surface surface, Light light)
 {
-    return saturate(dot(surface.normal, light.direction)) * light.color;
+    return
+		saturate(dot(surface.normal, light.direction) * light.attenuation) *
+		light.color;
 }
 
 float3 GetLighting(Surface surface, BRDF brdf, Light light)
@@ -11,12 +13,12 @@ float3 GetLighting(Surface surface, BRDF brdf, Light light)
     return IncomingLight(surface, light) * DirectBRDF(surface, brdf, light);
 }
 
-float3 GetLighting(Surface surface, BRDF brdf)
+float3 GetLighting(Surface surfaceWS, BRDF brdf)
 {
     float3 color = 0.0;
     for (int i = 0; i < GetDirectionalLightCount(); i++)
     {
-        color += GetLighting(surface, brdf, GetDirectionalLight(i));
+        color += GetLighting(surfaceWS, brdf, GetDirectionalLight(i, surfaceWS));
     }
     return color;
 }
