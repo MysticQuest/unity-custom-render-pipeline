38cd2adddf6ca6e8bf9228528a20dbf4b83f606f
Mystic Quest <l1skou@gmail.com>
2023-03-02 14:03:43 +0200

    adds dithered shadows to shadowcasterpass

--- Assets/Custom RP/Shaders/ShadowCasterPass.hlsl
+++ Assets/Custom RP/Shaders/ShadowCasterPass.hlsl
@@ -56,8 +56,11 @@ void ShadowCasterPassFragment(Varyings input)
     float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
     float4 base = baseMap * baseColor;
     // Removes any pixel that is outside of the view frustum
-#if defined(_CLIPPING)
+#if defined(_SHADOWS_CLIP)
 		clip(base.a - UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff));
+#elif defined(_SHADOWS_DITHER)
+		float dither = InterleavedGradientNoise(input.positionCS.xy, 0);
+		clip(base.a - dither);
 #endif
 }
 
