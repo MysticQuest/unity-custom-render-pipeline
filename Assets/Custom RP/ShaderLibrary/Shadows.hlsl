5808ecfcbb656e4757ee9868fe824a0c01c7e264
Mystic Quest <l1skou@gmail.com>
2023-03-02 13:04:03 +0200

    adds cascade blending by sampling from neighbouring cascade and lerping the shadow value between the two

--- Assets/Custom RP/ShaderLibrary/Shadows.hlsl
+++ Assets/Custom RP/ShaderLibrary/Shadows.hlsl
@@ -34,6 +34,7 @@ CBUFFER_END
 struct ShadowData
 {
     int cascadeIndex;
+    float cascadeBlend;
     float strength;
 };
 
@@ -45,6 +46,7 @@ float FadedShadowStrength(float distance, float scale, float fade)
 ShadowData GetShadowData(Surface surfaceWS)
 {
     ShadowData data;
+    data.cascadeBlend = 1.0;
     data.strength = FadedShadowStrength(
 		surfaceWS.depth, _ShadowDistanceFade.x, _ShadowDistanceFade.y
 	);
@@ -55,11 +57,16 @@ ShadowData GetShadowData(Surface surfaceWS)
         float distanceSqr = DistanceSquared(surfaceWS.position, sphere.xyz);
         if (distanceSqr < sphere.w)
         {
+            float fade = FadedShadowStrength(
+				distanceSqr, _CascadeData[i].x, _ShadowDistanceFade.z
+			);
             if (i == _CascadeCount - 1)
             {
-                data.strength *= FadedShadowStrength(
-					distanceSqr, _CascadeData[i].x, _ShadowDistanceFade.z
-				);
+                data.strength *= fade;
+            }
+            else
+            {
+                data.cascadeBlend = fade;
             }
             break;
         }
@@ -120,6 +127,18 @@ float GetDirectionalShadowAttenuation(DirectionalShadowData directional, ShadowD
 		float4(surfaceWS.position + normalBias, 1.0)
 	).xyz;
     float shadow = FilterDirectionalShadow(positionSTS);
+    if (global.cascadeBlend < 1.0)
+    {
+        normalBias = surfaceWS.normal *
+			(directional.normalBias * _CascadeData[global.cascadeIndex + 1].y);
+        positionSTS = mul(
+			_DirectionalShadowMatrices[directional.tileIndex + 1],
+			float4(surfaceWS.position + normalBias, 1.0)
+		).xyz;
+        shadow = lerp(
+			FilterDirectionalShadow(positionSTS), shadow, global.cascadeBlend
+		);
+    }
     return lerp(1.0, shadow, directional.strength);
 }
 
